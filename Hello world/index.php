bcab467ca3044fb5ab3c6ecda728c10b150b262c
deasidol <ava.sharipov@gmail.com>
2024-03-04 18:24:57 +0300

    закончил

--- Hello world/index.php
+++ Hello world/index.php
@@ -3,32 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, World!</title>
-    <style>
-        .header {
-            text-align: center;
-        }
-        .logo {
-            display: inline-block;
-            vertical-align: middle;
-            width: 100px;
-            height: auto;
-        }
-    </style>
+    <title>Hello World</title>
 </head>
+<style>
+    .header__wrapper{
+        margin: 0 auto 30px auto;
+        padding: 0px 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 100px;
+    }
+    .header__title{
+        font-size: 70px;
+        font-weight: bolder;
+    }
+    .footer__wrapper{
+        padding: 50px;
+        margin: 400px auto 0 auto;
+    }
+    .footer__text{
+        font-size: 40px;
+        text-align: center;
+    }
+</style>
 <body>
     <header class="header">
-        <img class="logo" src="img/logopolytech.png" alt="Логотип МосПолитеха">
-        <h1>Hello World!</h1>
+        <div class="header__wrapper">
+            <img src="img/logopolytech.png" alt="Логотип Московского Политеха" class="header__logo" width='200px' height='175px'>
+            <h1 class="header__title">Hello, World</h1>
+        </div>
     </header>
-
     <main>
-        <h2>Privet Mir</h2>
-        <p>Это динамический контент на PHP.</p>
+        <?php echo 'Hello, world!'?>
     </main>
-
-    <footer>
-        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.</p>
+    <footer class="footer">
+        <div class="footer__wrapper">
+            <p class="footer__text">Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.</p>
+        </div>
     </footer>
 </body>
 </html>
