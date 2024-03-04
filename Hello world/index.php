3f70ad60566e04ffbfdb68bec17a143db4f87b05
deasidol <ava.sharipov@gmail.com>
2024-03-04 17:52:05 +0300

    1лаба

--- Hello world/index.php
+++ Hello world/index.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hello, World!</title>
+    <style>
+        .header {
+            text-align: center;
+        }
+        .logo {
+            display: inline-block;
+            vertical-align: middle;
+            width: 100px;
+            height: auto;
+        }
+    </style>
 </head>
 <body>
-    
+    <header class="header">
+        <img class="logo" src="img/logopolytech.png" alt="Логотип МосПолитеха">
+        <h1>Hello World!</h1>
+    </header>
+
+    <main>
+        <h2>Privet Mir</h2>
+        <p>Это динамический контент на PHP.</p>
+    </main>
+
+    <footer>
+        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.</p>
+    </footer>
 </body>
 </html>
